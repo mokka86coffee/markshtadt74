31a0a3d08944851abdbf1e75c05bb2138146f0e3
mokka86coffee <mokka86coffee@gmail.com>
2019-04-22 20:15:44 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './index.scss';
 
-//? processing
 function crossBrowserFetch (url, optionObj = { on: 'GET', body: undefined }, JSONparsing = true) {
     return new Promise((resolve, reject) => {
         const req = new XMLHttpRequest();
@@ -40,7 +39,7 @@ function createNode(tag, attr, parentNode) {
     if ('className' in attr ) { node.className = attr['className'] }
     if ('src' in attr ) { node.setAttribute('src', attr['src']) }
     if ('text' in attr ) { node.innerHTML = attr['text'] }
-    parentNode.append(node);
+    parentNode.appendChild(node);
     return node;
 }
 
@@ -61,24 +60,17 @@ async function showMenu(modal, e) {
     let menuDivNode = document.querySelector('.modal-menu__content');
     modal.classList.add('modal--ready');
     menuDivNode.innerHTML = '';
-    try {
 
-        let menuObj = JSON.parse( localStorage.getItem('menusObj') )[path];
-        console.log(path)
-        console.log(menuObj)
-    
-        // ? localStorage.getItem('menusObj')[path] : await ajaxGetData(`bluda_id=${path}`); // main | banket | child
-        
-    
-        for (let key in menuObj) {
-            if (key === 'title') { document.querySelector('.modal-menu__info-title').innerText = menuObj['title']; continue; }
-            if (key === 'img') { document.querySelector('.modal-menu__img').setAttribute('src',menuObj['img']); continue; }
-            if (key === 'description') { document.querySelector('.modal-menu__info-text').innerText = menuObj['description']; continue; }
-            createNode('h4', {text: key}, menuDivNode);
-            mapMenuObjectToSpans(menuObj[key], menuDivNode);
-        }
-    } catch (err) {
-        console.log('error in showMenu', err);
+    let menuObj = JSON.parse( localStorage.getItem('menusObj') )[path];
+    console.log(path)
+    console.log(menuObj)
+
+    for (let key in menuObj) {
+        if (key === 'title') { document.querySelector('.modal-menu__info-title').innerText = menuObj['title']; continue; }
+        if (key === 'img') { document.querySelector('.modal-menu__img').setAttribute('src',menuObj['img']); continue; }
+        if (key === 'description') { document.querySelector('.modal-menu__info-text').innerText = menuObj['description']; continue; }
+        createNode('h4', {text: key}, menuDivNode);
+        mapMenuObjectToSpans(menuObj[key], menuDivNode);
     }
 
 }
@@ -116,8 +108,8 @@ function renderPhotos(e, parentDiv) {
     $('.modal--photos-halls').on(['touchstart','touchmove','touchend'], hallsPhTouchFn);
 }
 
-function chngIndex(right, quanity, index) {
-return right
+function chngIndex(toRight, quanity, index) {
+return toRight
     ? index +1 < quanity
         ? index +1  
         : 0
@@ -213,10 +205,10 @@ window.onload = async () => {
     $('.modal__close').on('click', (e) => e.target.parentNode.parentNode.classList.remove('modal--ready'));
     // modal hide on click
 
-    $('.services__item--about').on('click', showInfo.bind({}, $('.modal--info').node[0]));
+    $('.services__item--about').on('click', showInfo.bind(null, $('.modal--info').node[0]));
     // show/hide restaurant info
 
-    $('[data-href="contacts"]').on('click', showInfo.bind({}, $('.modal--contacts').node[0]));
+    $('[data-href="contacts"]').on('click', showInfo.bind(null, $('.modal--contacts').node[0]));
     // show/hide restaurant contacts
 
     $('.dishes__item, .footer-nav__column--menu').on('click', showMenu.bind(null, $('.modal--menu').node[0]));
@@ -224,7 +216,7 @@ window.onload = async () => {
 
 
 
-    $('.services__item--gallery, [data-href="gallery"]').on('click', showPhotos.bind({}, $('.modal--photos-halls').node[0]));
+    $('.services__item--gallery, [data-href="gallery"]').on('click', showPhotos.bind(null, $('.modal--photos-halls').node[0]));
     // show/hide restaurant halls photos
     
     $('[data-text]').on('click', e=>renderPhotos(e,hallsDiv) );
@@ -233,7 +225,7 @@ window.onload = async () => {
 }
 
 {
-    $('[data-href="news"]').on('click', showPhotos.bind({}, $('.modal--photos-news').node[0]));
+    $('[data-href="news"]').on('click', showPhotos.bind(null, $('.modal--photos-news').node[0]));
     // show/hide restaurant news photos
 
     const newsPhObj = { index: 0 };
@@ -250,7 +242,7 @@ window.onload = async () => {
 
 
 {  
-    $('[data-href="offers"]').on('click', showPhotos.bind({}, $('.modal--photos-offers').node[0]));
+    $('[data-href="offers"]').on('click', showPhotos.bind(null, $('.modal--photos-offers').node[0]));
     // show/hide restaurant offers photos
 
     const offersPhObj = { index: 0 };
@@ -280,16 +272,12 @@ window.onload = async () => {
     if ( !localStorage.getItem('menusObj') ) {
         Promise.all(promiseArr)
         .then( r => {
-            console.log(r);
-            localStorage.setItem('menusObj', JSON.stringify({ main: r[1], banket: r[0], child: r[2] })) 
+             localStorage.setItem('menusObj', JSON.stringify({ main: r[0], banket: r[1], child: r[2] })) 
         })
     }
     
-    await (async function (){ 
-        for (let node of document.getElementsByTagName('iframe')) { 
-            node.src = node.dataset.src;
-        } 
-    })();
+    const iframe = document.getElementsByTagName('iframe');
+    iframe.src = iframe.dataset.src;
 };
 
 var rublSVG = `<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 330 330" style="width: 10px;" xml:space="preserve"> <path id="XMLID_449_" d="M180,170c46.869,0,85-38.131,85-85S226.869,0,180,0c-0.183,0-0.365,0.003-0.546,0.01h-69.434 c-0.007,0-0.013-0.001-0.019-0.001c-8.284,0-15,6.716-15,15v0.001V155v45H80c-8.284,0-15,6.716-15,15s6.716,15,15,15h15v85 c0,8.284,6.716,15,15,15s15-6.716,15-15v-85h55c8.284,0,15-6.716,15-15s-6.716-15-15-15h-55v-30H180z M180,30.01 c0.162,0,0.324-0.003,0.484-0.008C210.59,30.262,235,54.834,235,85c0,30.327-24.673,55-55,55h-55V30.01H180z"/> </svg>`;
