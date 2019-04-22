705537233f5aaa858c089c1a53820ad4e3332ab0
mokka86coffee <mokka86coffee@gmail.com>
2019-04-22 13:21:33 +0300

    fixed path

--- src/index.js
+++ src/index.js
@@ -56,7 +56,7 @@ function mapMenuObjectToSpans (obj, menuDivNode) {
 async function showMenu(modal, e) {
     e.stopPropagation();
     e.preventDefault();
-    let path = e.path[0].dataset.menu ? e.path[0].dataset.menu : e.path[1].dataset.menu;
+    let path = e.target.dataset.menu;
     
     let menuDivNode = document.querySelector('.modal-menu__content');
     modal.classList.add('modal--ready');
@@ -219,7 +219,7 @@ window.onload = async () => {
     $('[data-href="contacts"]').on('click', showInfo.bind({}, $('.modal--contacts').node[0]));
     // show/hide restaurant contacts
 
-    $('.dishes__item, .footer-nav__column--menu').on('click', showMenu.bind({}, $('.modal--menu').node[0]));
+    $('.dishes__item, .footer-nav__column--menu').on('click', showMenu.bind(null, $('.modal--menu').node[0]));
     // show/hide restaurant menu
 
 
@@ -271,12 +271,6 @@ window.onload = async () => {
     
     renderPhotos = renderPhotos.bind(hallsObj);
     renderPhotos(null, hallsDiv);
-    
-    await (async function (){ 
-        for (let node of document.getElementsByTagName('iframe')) { 
-            node.src = node.dataset.src;
-        } 
-    })();
 
     const promiseArr = [
         crossBrowserFetch(`functions.php?bluda_id=main`),
@@ -290,6 +284,12 @@ window.onload = async () => {
             localStorage.setItem('menusObj', JSON.stringify({ main: r[1], banket: r[0], child: r[2] })) 
         })
     }
+    
+    await (async function (){ 
+        for (let node of document.getElementsByTagName('iframe')) { 
+            node.src = node.dataset.src;
+        } 
+    })();
 };
 
 var rublSVG = `<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 330 330" style="width: 10px;" xml:space="preserve"> <path id="XMLID_449_" d="M180,170c46.869,0,85-38.131,85-85S226.869,0,180,0c-0.183,0-0.365,0.003-0.546,0.01h-69.434 c-0.007,0-0.013-0.001-0.019-0.001c-8.284,0-15,6.716-15,15v0.001V155v45H80c-8.284,0-15,6.716-15,15s6.716,15,15,15h15v85 c0,8.284,6.716,15,15,15s15-6.716,15-15v-85h55c8.284,0,15-6.716,15-15s-6.716-15-15-15h-55v-30H180z M180,30.01 c0.162,0,0.324-0.003,0.484-0.008C210.59,30.262,235,54.834,235,85c0,30.327-24.673,55-55,55h-55V30.01H180z"/> </svg>`;
